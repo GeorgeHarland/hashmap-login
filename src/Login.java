026aaa93831ba70e27fd4452689c0356895c58e0
George Harland <georgeharland12@gmail.com>
2022-01-01 23:29:07 +0000

    Added titles

--- src/Login.java
+++ src/Login.java
@@ -15,6 +15,7 @@ public class Login implements ActionListener{
     JTextField usernameInput = new JTextField();
     JPasswordField passInput = new JPasswordField();
     JLabel validLabel = new JLabel();
+    JLabel titleLabel = new JLabel();
     HashMap<String,String> infoCopy = new HashMap<String,String>();
 
     Login(HashMap<String,String> loginInfo){
@@ -28,6 +29,11 @@ public class Login implements ActionListener{
         validLabel.setFont(new Font(null,Font.ITALIC,20));
         validLabel.setForeground(Color.DARK_GRAY);
 
+        titleLabel.setBounds(50,30,400,35);
+        titleLabel.setFont(new Font(null,Font.ITALIC,28));
+        titleLabel.setForeground(Color.DARK_GRAY);
+        titleLabel.setText("Login-Systems");
+
         usernameInput.setBounds(125,100,200,25);
         passInput.setBounds(125,150,200,25);
 
@@ -38,6 +44,7 @@ public class Login implements ActionListener{
         frame.add(usernameLabel);
         frame.add(passLabel);
         frame.add(validLabel);
+        frame.add(titleLabel);
         frame.add(usernameInput);
         frame.add(passInput);
         frame.add(loginButton);
